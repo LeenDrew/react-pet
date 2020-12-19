4b836506e4d76d6d2fd7f423d7d22d988e82c796
IndoorGhoul <hermione1granger121@gmail.com>
2020-12-19 12:32:57 +0600

    a bit of work

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/css/Header.css';
+
+function Header() {
+  return (
+    <header>
+      <div>123</div>
+    </header>
+  );
+}
+
+export default Header;
